0b61229947060f1c069722e2c2abe067d4c13ca9
Sandeep Singh <sandeep.singh@RIPL.RICOH.COM>
2017-04-27 16:00:48 +0530

    added test2 file

---
+++ src/test/java/com/Rest/Test2.java
@@ -0,0 +1,16 @@
+package com.Rest;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	@Test
+	public void TestCase1()
+	{
+		System.out.println("Verify the log details of the web services : - ");
+		
+	}
+
+}
